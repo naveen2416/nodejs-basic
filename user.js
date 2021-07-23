7720be54033e5db7784b6d55202c1f81ff09ed00
naveen2416 <naveenbala695@gmail.com>
2021-07-24 01:46:39 +0530

    complete a nodejs basic task

---
+++ user.js
@@ -0,0 +1,5 @@
+const result = require("./common");
+console.log(`The addition value is ${result.add(12,12)}`);
+console.log(`The subraction value is ${result.sub(24,12)}`);
+console.log(`The multiplication value is ${result.mul(3,12)}`);
+console.log(`The division value is ${result.div(120,12)}`);
